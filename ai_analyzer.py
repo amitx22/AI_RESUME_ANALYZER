d94fb0aaa05eb68d30a71002eef7b219b821bc4f
AMIT KUMAR SINGH <168517384+amitx22@users.noreply.github.com>
2026-06-10 02:03:33 +0530

    Update ai_analyzer.py

--- ai_analyzer.py
+++ ai_analyzer.py
@@ -1,11 +1,4 @@
 import google.generativeai as genai
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-genai.configure(api_key=os.getenv("AQ.Ab8RN6IGZdaIZk53lh9LNJd7m50uIR4Li-Mbw1Va3stq20ylxw"))
-
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def analyze_resume(resume_text):
